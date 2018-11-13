01540b66fd4271055cbd94f391ab6259c074503d
jeffe8 <salazarj@seattleu.edu>
2018-11-13 11:07:11 -0800

    vue analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueLocalStorage from 'vue-ls';
+import VueLocalStorage from 'vue-ls'
+import VueAnalytics from 'vue-analytics';
 
 let options = {
   namespace: 'book__'
